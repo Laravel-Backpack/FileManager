e1a37bd4ed04d1e24f8a1a37e1e4f98693d0bcd4
Jesper Noordsij <jesper@sslleiden.nl>
2020-05-20 12:39:59 +0200

    Translate File Manager heading and breadcrumb in views/elfinder.blade.php

--- resources/views/elfinder.blade.php
+++ resources/views/elfinder.blade.php
@@ -28,13 +28,13 @@
 @php
   $breadcrumbs = [
     trans('backpack::crud.admin') => url(config('backpack.base.route_prefix'), 'dashboard'),
-    'File Manager' => false,
+    trans('backpack::crud.file_manager') => false,
   ];
 @endphp
 
 @section('header')
     <section class="container-fluid">
-      <h2>File Manager</h2>
+      <h2>{{ trans('backpack::crud.file_manager') }}</h2>
     </section>
 @endsection
 
