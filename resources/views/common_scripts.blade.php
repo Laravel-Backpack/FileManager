27e5a742d68095aee74aa9674ea6ade5da070b8f
AfzalSabbir <afzalbd1@gmail.com>
2022-06-16 11:25:55 +0600

    Update common_scripts.blade.php
    jqueryui/1.10.4 to jqueryui/1.13.1
    
    Because some of designs were breaking which solved in this new version and elFinder itself used this new jQueryUI.

--- resources/views/common_scripts.blade.php
+++ resources/views/common_scripts.blade.php
@@ -1,9 +1,9 @@
         <!-- jQuery and jQuery UI (REQUIRED) -->
-        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+        <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.13.1/themes/smoothness/jquery-ui.css" />
         @if (!isset ($jquery) || (isset($jquery) && $jquery == true))
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         @endif
-        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.13.1/jquery-ui.min.js"></script>
 
         <!-- elFinder JS (REQUIRED) -->
         <script src="{{ asset($dir.'/js/elfinder.min.js') }}"></script>
