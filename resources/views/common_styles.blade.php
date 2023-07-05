3dcb907216a4213b974692108a200c125e526f6c
Jesper Noordsij <jesper.noordsij@gmail.com>
2023-07-05 12:46:20 +0200

    Switch from using .zip to .tar.gz for basset archives

--- resources/views/common_styles.blade.php
+++ resources/views/common_styles.blade.php
@@ -2,7 +2,7 @@
         <title>File Manager</title>
 
         {{-- elFinder CSS (REQUIRED) --}}
-        @bassetArchive('https://github.com/Studio-42/elFinder/archive/refs/tags/2.1.61.zip', 'elfinder-2.1.61')
+        @bassetArchive('https://github.com/Studio-42/elFinder/archive/refs/tags/2.1.61.tar.gz', 'elfinder-2.1.61')
         @basset('elfinder-2.1.61/elFinder-2.1.61/css/elfinder.min.css')
 
         {{-- elFinder Backpack Theme --}}
