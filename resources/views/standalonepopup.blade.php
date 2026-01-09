8a997bad51fb60753d3b0f0648a01ab94e8fe803
Pedro Martins <pxpm88@gmail.com>
2026-01-09 12:00:40 +0000

    fix languages in elfinder (#81)

--- resources/views/standalonepopup.blade.php
+++ resources/views/standalonepopup.blade.php
@@ -1,7 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        @include('backpack.filemanager::localization')
+        @php
+            $elfinderLanguages = [
+                'ar', 'bg', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 'fa', 'fo', 'fr', 'fr_CA', 'he', 'hr', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'no', 'pl', 'pt_BR', 'ro', 'ru', 'si', 'sk', 'sl', 'sr', 'sv', 'tr', 'ug_CN', 'uk', 'vi', 'zh_CN', 'zh_TW',
+            ];
+            $locales = config('backpack.crud.locales', []);
+            $elfinderConfiguredLanguages = [];
+            foreach ($locales as $code => $name) {
+                if (in_array($code, $elfinderLanguages)) {
+                    $elfinderConfiguredLanguages[$code] = $name;
+                }
+            }
+            // Add English if not present, as it's the fallback
+            if (!array_key_exists('en', $elfinderConfiguredLanguages)) {
+                    $elfinderConfiguredLanguages['en'] = 'English';
+            }
+        @endphp
         @include('backpack.filemanager::common_scripts', ['locale' => in_array($locale, array_keys($elfinderConfiguredLanguages)) ? $locale : null])
         @include('backpack.filemanager::common_styles', ['styleBodyElement' => true])
         <style type="text/css">
