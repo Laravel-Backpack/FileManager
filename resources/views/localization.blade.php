e5a969843a0bb08f99d01d70b84b92ed509ebe7a
pxpm <pxpm88@gmail.com>
2026-01-09 12:00:00 +0000

    fix languages in elfinder

--- resources/views/localization.blade.php
+++ resources/views/localization.blade.php
@@ -1,18 +1,5 @@
 @php
-    $elfinderLanguages = [
-        'ar', 'bg', 'ca', 'cs', 'da', 'de', 'el', 'en', 'es', 'fa', 'fo', 'fr', 'fr_CA', 'he', 'hr', 'hu', 'id', 'it', 'ja', 'ko', 'nl', 'no', 'pl', 'pt_BR', 'ro', 'ru', 'si', 'sk', 'sl', 'sr', 'sv', 'tr', 'ug_CN', 'uk', 'vi', 'zh_CN', 'zh_TW',
-    ];
-    $locales = config('backpack.crud.locales', []);
-    $elfinderConfiguredLanguages = [];
-    foreach ($locales as $code => $name) {
-        if (in_array($code, $elfinderLanguages)) {
-            $elfinderConfiguredLanguages[$code] = $name;
-        }
-    }
-    // Add English if not present, as it's the fallback
-    if (!array_key_exists('en', $elfinderConfiguredLanguages)) {
-            $elfinderConfiguredLanguages['en'] = 'English';
-    }
+
 
     // Pre-load other configured languages so they are available when switching in UI
     foreach ($elfinderConfiguredLanguages as $lang => $name) {
